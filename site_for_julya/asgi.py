9ddc6d4103f85a95ac32b33882dc6423fe6f1b55
Dania <aooa32544@gmail.com>
2024-08-21 21:50:50 -0400

    start project

---
+++ site_for_julya/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for site_for_julya project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_for_julya.settings')
+
+application = get_asgi_application()
